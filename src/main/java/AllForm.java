4884f7baf04e13901de820b5c54781cb0459ec34
Yurii17K <yura11231@gmail.com>
2021-01-10 13:57:16 +0200

    read/delete cookies, session track

--- src/main/java/AllForm.java
+++ src/main/java/AllForm.java
@@ -16,32 +16,43 @@ public class AllForm extends HttpServlet {
         // Set response content type
         response.setContentType("text/html");
 
-        // Get current time
-        Calendar calendar = new GregorianCalendar();
-        String am_pm;
-        int hour = calendar.get(Calendar.HOUR);
-        int minute = calendar.get(Calendar.MINUTE);
-        int second = calendar.get(Calendar.SECOND);
+        // Create cookies for first and last names.
+        Cookie firstName = new Cookie("first_name", request.getParameter("first_name"));
+        Cookie lastName = new Cookie("last_name", request.getParameter("last_name"));
 
-        if(calendar.get(Calendar.AM_PM) == 0)
-            am_pm = "AM";
-        else
-            am_pm = "PM";
+        // Set expiry date after 24 Hrs for both the cookies.
+        firstName.setMaxAge(60*60*24);
+        lastName.setMaxAge(60*60*24);
 
-        String CT = hour+":"+ minute +":"+ second +" "+ am_pm;
+        // Add both the cookies in the response header.
+        response.addCookie( firstName );
+        response.addCookie( lastName );
+
+        // Set response content type
+        response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
-        String title = "Auto Refresh Header Setting";
+        String title = "Setting Cookies Example";
         String docType =
                 "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
 
         out.println(docType +
                 "<html>\n" +
-                "<head><title>" + title + "</title></head>\n"+
-                "<body bgcolor = \"#f0f0f0\">\n" +
-                "<h1 align = \"center\">" + title + "</h1>\n" +
-                "<p>Current Time is: " + CT + "</p>\n"
-        );
+                "<head>" +
+                "<title>" + title + "</title>" +
+                "</head>\n" +
+                    "<body bgcolor = \"#f0f0f0\">\n" +
+                        "<h1 align = \"center\">" + title + "</h1>\n" +
+                        "<br>" +
+                        "<ul>\n" +
+                        "  <li><b>First Name</b>: "
+                        + request.getParameter("first_name") + "\n" +
+                        "  <li><b>Last Name</b>: "
+                        + request.getParameter("last_name") + "\n" +
+                        "</ul>\n" +
+                    "</body>" +
+                "</html>"
+      );
     }
 
     @Override
